4c6690dd7b4355980bc0a0122cc4f6a51422b6b5
Studio L22 <studio@l22.local>
2026-04-21 15:47:59 +0200

    Setup GitHub Pages deployment

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   transpilePackages: ['@studio-l22/ui', '@studio-l22/core', '@studio-l22/types', '@studio-l22/config'],
 }
 
